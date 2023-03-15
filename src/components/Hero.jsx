a26e1e4f260e5df8e3b31a64e4d822f34accda4c
Ion Stici <ionsticidev@gmail.com>
2023-03-15 13:46:04 +0200

    Finish styling for hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import imgIntroMobile from './../imgs/image-intro-mobile.jpg';
 import imgIntroDesktop from './../imgs/image-intro-desktop.jpg';
@@ -9,57 +9,68 @@ import bgPatternIntroLeftDesktop from './../imgs/bg-pattern-intro-left-desktop.s
 import bgPatternIntroRightMobile from './../imgs/bg-pattern-intro-right-mobile.svg';
 import bgPatternIntroRightDesktop from './../imgs/bg-pattern-intro-right-desktop.svg';
 
-export const Hero = function (props) {
-    const mediaMatch = window.matchMedia('(min-width: 600px)');
-    const [matches, setMatches] = React.useState(mediaMatch.matches);
+export const Hero = function () {
+    const mediaMatch600px = window.matchMedia('(min-width: 600px)');
+    const mediaMatch768px = window.matchMedia('(min-width: 768px)');
+
+    const [matches600px, setMatches600px] = useState(mediaMatch600px.matches);
+    const [matches768px, setMatches768px] = useState(mediaMatch768px.matches);
 
     useEffect(() => {
-        const handler = e => setMatches(e.matches);
-        mediaMatch.addEventListener('change', handler);
-        console.log(matches);
-        return () => mediaMatch.removeEventListener('change', handler);
+        const handler600px = e => setMatches600px(e.matches);
+        const handler768px = e => setMatches768px(e.matches);
+
+        mediaMatch600px.addEventListener('change', handler600px);
+        mediaMatch768px.addEventListener('change', handler768px);
+
+        return () => {
+            mediaMatch600px.removeEventListener('change', handler600px);
+            mediaMatch768px.removeEventListener('change', handler768px);
+        };
     });
 
     return (
         <header className="header">
-            <div className="header__img-box">
-                <img
-                    className="header__hero-img"
-                    src={matches ? imgIntroDesktop : imgIntroMobile}
-                    alt="Happy Family"
-                />
-            </div>
+            <img
+                className="header__left-pattern"
+                src={
+                    matches768px
+                        ? bgPatternIntroLeftDesktop
+                        : bgPatternIntroLeftMobile
+                }
+                alt=""
+            />
+            <img
+                className="header__right-pattern"
+                src={
+                    matches768px
+                        ? bgPatternIntroRightDesktop
+                        : bgPatternIntroRightMobile
+                }
+                alt=""
+            />
 
-            <div className="header__content-box">
-                <img
-                    className="header__left-pattern"
-                    src={
-                        matches
-                            ? bgPatternIntroLeftDesktop
-                            : bgPatternIntroLeftMobile
-                    }
-                    alt=""
-                />
-                <img
-                    className="header__right-pattern"
-                    src={
-                        matches
-                            ? bgPatternIntroRightDesktop
-                            : bgPatternIntroRightMobile
-                    }
-                    alt=""
-                />
+            <div className="header__wrapper">
+                <div className="header__img-box">
+                    <img
+                        className="header__hero-img"
+                        src={matches600px ? imgIntroDesktop : imgIntroMobile}
+                        alt="Happy Family"
+                    />
+                </div>
 
-                <h1 className="h1">Humanizing your insurance.</h1>
-                <p className="p">
-                    Get your life insurance coverage easier and faster. We blend
-                    our expertise and technology to help you find the plan
-                    that’s right for you. Ensure you and your loved ones are
-                    protected.
-                </p>
-                <a className="a" href="./view-plans">
-                    View plans
-                </a>
+                <div className="header__content-box">
+                    <h1 className="h1">Humanizing your insurance.</h1>
+                    <p className="p">
+                        Get your life insurance coverage easier and faster. We
+                        blend our expertise and technology to help you find the
+                        plan that’s right for you. Ensure you and your loved
+                        ones are protected.
+                    </p>
+                    <a className="hero-link" href="./view-plans">
+                        View plans
+                    </a>
+                </div>
             </div>
         </header>
     );
