14e2375117bec37da9b4de02f1c209303446df84
Ion Stici <ionsticidev@gmail.com>
2023-03-15 14:01:52 +0200

    Add features section

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,20 @@
 import { NavBar } from './components/NavBar';
 import { Hero } from './components/Hero';
+import { Features } from './components/Features';
 
 function App() {
     return (
         <>
             <NavBar links={['How we work', 'Blog', 'Account', 'View plans']} />
             <Hero />
+            <main>
+                <Features />
+            </main>
         </>
     );
 }
 
 export default App;
-
-//   We’re different
-
-//   Snappy Process
-
-//   Our application process can be completed in minutes, not hours. Don’t get
-//   stuck filling in tedious forms.
-
-//   Affordable Prices
-
-//   We don’t want you worrying about high monthly costs. Our prices may be low,
-//   but we still offer the best coverage possible.
-
-//   People First
-
-//   Our plans aren’t full of conditions and clauses to prevent payouts. We make
-//   sure you’re covered when you need it.
 
 //   Find out more about how we work
 
